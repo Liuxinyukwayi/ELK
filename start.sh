2e268da84c94eb548d38e9e652eb0f69877411d1
Liuxinyukwayi <1638332955@qq.com>
2025-09-30 15:04:08 +0800

    lts

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 #安装ansible
-#bash ansible-install.sh
+bash ansible-install.sh
 
 #配置sudo免密码
-ansible-playbook -i elk-ansible/hosts.ini sudoers.yml --ask-become-pass
+ansible-playbook -i elk-ansible/hosts.ini udosers.yml --ask-become-pass
 # 语法检查
 ansible-playbook -i elk-ansible/hosts.ini --syntax-check site.yml
 # 先跑一次（可加 -C 进行检查模式）
